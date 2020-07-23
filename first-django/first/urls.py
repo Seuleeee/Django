cf8a10854278b6ead1012a9fbef9d57d8a86897b
Seuleeee <jhs931224@gmail.com>
2020-07-24 03:33:16 +0900

    2020 0724 {% load static %} 때문에 힘들었음. 그래도 해결해서 다행

--- first-django/first/urls.py
+++ first-django/first/urls.py
@@ -4,8 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),#앞의 ''은 url치고 들어오자마자 라는 뜻임. first라는 package 안의 view라는 python파일의 index라는 method를 연결할것임.
-    path('select/', views.select, name='select'),
-    path('result/', views.result, name='result'),
+    path('select/', views.select, name="select"),
+    path('result/', views.result, name="result"),
 
     # path('select/<int:year>/', .., ..) #숫자가 들어오면 year로 받아서 넣어줘라. ex select/2019/ int, slug, str 등 datatype으로 보내기 가능
 ]
--- first-django/first/views.py
+++ first-django/first/views.py
@@ -3,22 +3,41 @@ from django.http import HttpResponse
 
 from django.template import loader #html 불러오려면 import
 from datetime import datetime
+import random
 
 # Create your views here.
 # page하나에 method 하나
 
 def index(request):
-    template = loader.get_template('index.html')
+    # template = loader.get_template('index.html')
     now = datetime.now()
     context = {
         'current_date': now
     }
-    return HttpResponse(template.render(context, request))
+    return render(request, 'first/index.html', context)
 
-def select(request, year):
-    message = 'Select deeplearning model.'
-    return HttpResponse(message)
+def select(request):
+    context = {}
+    return render(request, 'first/select.html', context)
 
 def result(request):
-    message = 'result of deeplearning'
-    return HttpResponse(message)
+    chosen = int(request.GET['number'])
+    results = []
+    if chosen >= 1 and chosen <=45:
+        results.append(chosen)
+
+    box = []
+    for i in range(0,45):
+        if chosen != i+1:
+            box.append(i+1)
+
+    random.shuffle(box)
+
+    while len(results) < 6:
+        results.append(box.pop())
+
+
+    context = {
+        'numbers':results
+    }
+    return render(request, 'first/result.html', context)
